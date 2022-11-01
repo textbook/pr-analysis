a76c432e349521b6e0c04685e7c9ddd344124891
Jonathan Sharpe <jonathan.sharpe@consultant.lego.com>
2022-11-01 11:33:50 +0000

    Support filtering by date
    Allows analysing on e.g. a sprint basis.

--- cli.py
+++ cli.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+import datetime
 import sys
 
 from core import get_merged_pull_requests
@@ -8,10 +9,20 @@ from stats import describe
 
 def get_options(args: list[str]) -> argparse.Namespace:
     parser = argparse.ArgumentParser()
+    parser.add_argument("--created-after", default=None, help="Filter by creation date", type=_valid_date)
+    parser.add_argument("--limit", default=None, help="Number of PRs to analyse", type=int)
+    parser.add_argument("--merged-before", default=None, help="Filter by merge date", type=_valid_date)
     parser.add_argument("--owner", help="Org or user", required=True, type=str)
     parser.add_argument("--repo", help="Repository", required=True, type=str)
-    parser.add_argument("--limit", default=None, help="Number of PRs to analyse", type=int)
     return parser.parse_args(args)
+
+
+def _valid_date(value: str) -> datetime.datetime:
+    try:
+        return datetime.datetime.strptime(value, "%Y-%m-%d")
+    except ValueError:
+        msg = "not a valid date: {0!r}".format(value)
+        raise argparse.ArgumentTypeError(msg)
 
 
 if __name__ == "__main__":
--- core.py
+++ core.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import typing
 
@@ -31,14 +32,21 @@ def closed_pull_requests(*, owner: str, repo: str) -> typing.Iterable[PullReques
 
 def get_merged_pull_requests(
     *,
+    created_after: datetime.datetime | None,
     limit: int | None,
+    merged_before: datetime.datetime | None,
     owner: str,
     repo: str,
 ) -> list[PullRequest]:
     """Get a list of all merged Pull Requests in the specified repo."""
     pull_requests = []
     for pull_request in closed_pull_requests(owner=owner, repo=repo):
-        if pull_request["merged_at"] is not None:
+        if created_after is not None and pull_request["created_at"] < _isoformat(created_after):
+            break
+        if (
+            (merged_at := pull_request["merged_at"]) is not None
+            and (merged_before is None or merged_at < _isoformat(merged_before))
+        ):
             pull_requests.append(pull_request)
         if limit is not None and len(pull_requests) == limit:
             break
@@ -54,3 +62,7 @@ def _get_next_page(header: str) -> str | None:
         )
     }
     return links.get("next")
+
+
+def _isoformat(dt: datetime.datetime) -> str:
+    return f"{dt.isoformat(timespec='milliseconds')}Z"
