d09358171cc2b675825ed06342ede82fd646750b
Jonathan Sharpe <jonathan.sharpe@consultant.lego.com>
2022-11-01 14:06:21 +0000

    Move hours open to core to include in JSON

--- core.py
+++ core.py
@@ -3,6 +3,10 @@ import datetime
 from api import merged_pull_requests, PullRequest
 
 
+class EnrichedPullRequest(PullRequest):
+    open_for: int
+
+
 def get_merged_pull_requests(
     *,
     created_after: datetime.datetime | None,
@@ -11,14 +15,14 @@ def get_merged_pull_requests(
     owner: str,
     repo: str,
     **_,
-) -> list[PullRequest]:
+) -> list[EnrichedPullRequest]:
     """Get a list of relevant merged Pull Requests in the specified repo."""
     pull_requests = []
     for pull_request in merged_pull_requests(owner=owner, repo=repo):
         if created_after is not None and pull_request["created_at"] < _isoformat(created_after):
             break
         if merged_before is None or pull_request["merged_at"] < _isoformat(merged_before):
-            pull_requests.append(pull_request)
+            pull_requests.append(dict(open_for=_open_for(pull_request), **pull_request))
         if limit is not None and len(pull_requests) == limit:
             break
     return pull_requests
@@ -26,3 +30,11 @@ def get_merged_pull_requests(
 
 def _isoformat(dt: datetime.datetime) -> str:
     return f"{dt.isoformat(timespec='milliseconds')}Z"
+
+
+def _open_for(pull_request: PullRequest) -> int:
+    """Time PR was open, in hours."""
+    iso_format = "%Y-%m-%dT%H:%M:%SZ"
+    created = datetime.datetime.strptime(pull_request["created_at"], iso_format)
+    closed = datetime.datetime.strptime(pull_request["closed_at"], iso_format)
+    return round((closed - created).total_seconds() / (60 * 60))
--- stats.py
+++ stats.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import statistics
 import typing
 
-from core import PullRequest
+from core import EnrichedPullRequest
 
 
 class Stats(typing.TypedDict):
@@ -11,14 +11,6 @@ class Stats(typing.TypedDict):
     mode: float
 
 
-def describe(pull_requests: list[PullRequest]) -> Stats:
-    lives = [_open_for(pr) for pr in pull_requests]
+def describe(pull_requests: list[EnrichedPullRequest]) -> Stats:
+    lives = [pr["open_for"] for pr in pull_requests]
     return dict(mean=statistics.mean(lives), median=statistics.median(lives), mode=statistics.mode(lives))
-
-
-def _open_for(pull_request: PullRequest) -> int:
-    """Time PR was open, in hours."""
-    iso_format = "%Y-%m-%dT%H:%M:%SZ"
-    created = datetime.strptime(pull_request["created_at"], iso_format)
-    closed = datetime.strptime(pull_request["closed_at"], iso_format)
-    return round((closed - created).total_seconds() / (60 * 60))
